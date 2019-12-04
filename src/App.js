086d185a39a321a705d09387a27ee55b64c36434
shrki416 <coach-ahmed@bmsnj.org>
2019-12-04 13:31:27 -0500

    home page styled

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 
-import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="header">
+          <p>Home</p>
+          <p>Page 1</p>
+          <p>Page 2</p>
+      </div>
+      <div className="cover">
+        <h1>Twitter Showcase</h1>
+        <p>
+          Allows users to search for a twitter username or by tweet content from
+          the last week.
+        </p>
+      </div>
+    </div>
+  );
+}
