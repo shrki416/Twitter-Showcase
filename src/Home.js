9f48dec2ce1ed3a8b32d47c7f6c4129cf6750dd6
shrki416 <coach-ahmed@bmsnj.org>
2019-12-04 21:20:11 -0500

    add react-router

--- src/Home.js
+++ src/Home.js
@@ -8,13 +8,13 @@ export default function Home() {
           <i className="fab fa-twitter-square"></i>
         </p>
         <p>Home</p>
-        <p>Page 1</p>
-        <p>Page 2</p>
+        <p>User</p>
+        <p>Random</p>
       </div>
       <div className="cover">
         <h1>Twitter Showcase</h1>
         <p>
-          Allows users to search for a twitter username or by tweet content from
+          Allows users to search for tweets using a twitter username or by tweet content from
           the last week.
         </p>
       </div>
