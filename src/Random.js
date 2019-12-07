05eb245eb839c7101d0de84c17cfadff11fce383
shrki416 <coach-ahmed@bmsnj.org>
2019-12-07 12:47:21 -0500

    up to date

--- src/Random.js
+++ src/Random.js
@@ -9,18 +9,21 @@ const MLS = require('./assets/MLS.png')
 const NBA = require('./assets/NBA.png')
 
 function Random() {
+  
+  function handleClick(e) {
+    alert('image clicked!')
+  }
+
     return (
       <>
         <Navbar />
         <h1 className="random-header">Random Tweet Page</h1>
         <section className="random-container">
-          <img 
-            onClick={() => alert('clicked!')} 
-            className="random-img" src={MLB} alt="MLB logo" />
-          <img className="random-img" src={NHL} alt="NHL logo" />
-          <img className="random-img" src={NBA} alt="NBA logo" />
-          <img className="random-img" src={MLS} alt="MLS lgoo" />
-          <img className="random-img" src={NFL} alt="NFL logo" />
+          <img onClick={handleClick} className="random-img" src={MLB} alt="MLB logo" />
+          <img onClick={handleClick} className="random-img" src={NHL} alt="NHL logo" />
+          <img onClick={handleClick} className="random-img" src={NBA} alt="NBA logo" />
+          <img onClick={handleClick} className="random-img" src={MLS} alt="MLS lgoo" />
+          <img onClick={handleClick} className="random-img" src={NFL} alt="NFL logo" />
         </section>
       </>
     );
