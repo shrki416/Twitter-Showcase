c17a2a354da5c50b57ab5ee0ae9dcda46ba2e368
shrki416 <coach-ahmed@bmsnj.org>
2019-12-06 20:05:58 -0500

    up to date

--- src/Random.js
+++ src/Random.js
@@ -6,8 +6,12 @@ function Random() {
     return (
       <>
         <Navbar />
+        <h1 className="random-header">Random Tweet Page</h1>
         <section className="random-container">
-          <h1 className="random-header">Random Tweet Page</h1>
+          <input type="text" className="search-input" placeholder="Search"/> 
+          <br />
+          <button className="btn btn-user" type="submit">Search By User</button>
+          <button className="btn btn-handle" type="submit">Search By Twitter Handle</button>
         </section>
       </>
     );
