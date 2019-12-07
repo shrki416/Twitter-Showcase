c69b9b0c7e310fe503342cf3af45a0836e62a11c
shrki416 <coach-ahmed@bmsnj.org>
2019-12-06 22:05:52 -0500

    up to date

--- src/Random.js
+++ src/Random.js
@@ -2,16 +2,23 @@ import React from 'react';
 import Navbar from './Navbar';
 import './Random.css';
 
+const MLB = require('./assets/MLB.png')
+const NHL = require('./assets/NHL.png')
+const NFL = require('./assets/NFL.png')
+const MLS = require('./assets/MLS.png')
+const NBA = require('./assets/NBA.png')
+
 function Random() {
     return (
       <>
         <Navbar />
         <h1 className="random-header">Random Tweet Page</h1>
         <section className="random-container">
-          <input type="text" className="search-input" placeholder="Search"/> 
-          <br />
-          <button className="btn btn-user" type="submit">Search By User</button>
-          <button className="btn btn-handle" type="submit">Search By Twitter Handle</button>
+          <img src={MLB} alt="MLB logo" />
+          <img src={NHL} alt="NHL logo" />
+          <img src={NFL} alt="NFL logo" />
+          <img src={MLS} alt="MLS lgoo" />
+          <img src={NBA} alt="NBA logo" />
         </section>
       </>
     );
