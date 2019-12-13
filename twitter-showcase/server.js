9bc29f5023efb845fe60b203a1688ae08fda2a37
shrki416 <coach-ahmed@bmsnj.org>
2019-12-12 21:29:10 -0500

    install new dependencies

--- twitter-showcase/server.js
+++ twitter-showcase/server.js
@@ -3,6 +3,6 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Connected!"));
 
 app.listen(port, () => console.log(`Server Started on port ${port}!`));
