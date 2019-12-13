3607bac24216a84453068e0051c46e09aa284e8c
shrki416 <coach-ahmed@bmsnj.org>
2019-12-12 20:55:37 -0500

    backend started

---
+++ twitter-showcase/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => console.log(`Server Started on port ${port}!`));
