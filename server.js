c3b8c1ffbdaae711ae626ad33a8511f4e8d1311c
shrki416 <coach-ahmed@bmsnj.org>
2019-12-13 21:31:37 -0500

    add dependencies, set-up server to serve react files

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const express = require("express");
+const express   = require("express");
+const axios     = require('axios');
+const path      = require('path');
+
+require('dotenv').config();
+const API_KEY = process.env.API_KEY;
+const API_SECRET_KEY = process.env.API_SECRET_KEY;
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Server Started on port ${port}!`));
