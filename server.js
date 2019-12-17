51653ce8006ca8493e34ff7405212671dd57c4ad
shrki416 <coach-ahmed@bmsnj.org>
2019-12-17 15:16:12 -0500

    Able to get a tweet logged to the console

--- server.js
+++ server.js
@@ -5,12 +5,30 @@ const path = require("path");
 require("dotenv").config();
 const API_KEY = process.env.API_KEY;
 const API_SECRET_KEY = process.env.API_SECRET_KEY;
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
-// app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/search", (req, res) => {
+  const url =
+    "https://api.twitter.com/1.1/search/tweets.json?q=nfl&result_type=popular";
+
+  const config = {
+    headers: {
+      Authorization: `Bearer ${ACCESS_TOKEN}`
+    }
+  };
+  axios
+    .get(url, config)
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 app.listen(port, () => console.log(`Server Started on port ${port}!`));
