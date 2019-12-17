c14184737e841b21ff00da2985a2f356b3bfd7ed
shrki416 <coach-ahmed@bmsnj.org>
2019-12-17 13:02:08 -0500

    Added cra-build-watch dev dependency

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import './App.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-    return (
-        <nav className="main-nav">
-          <ul>
-            <li>
-              <i className="fab fa-twitter-square fa-7x"></i>
-            </li>
-            <Link to="/">
-              <li>Home</li>
-            </Link>
-            <Link to="/search">
-              <li>Search</li>
-            </Link>
-            <Link to="/random">
-              <li>Random</li>
-            </Link>
-          </ul>
-        </nav>
-    );
+  return (
+    <nav className="main-nav">
+      <ul>
+        <li>
+          <i className="fab fa-twitter-square fa-5x"></i>
+        </li>
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/search">
+          <li>Search</li>
+        </Link>
+        <Link to="/random">
+          <li>Random</li>
+        </Link>
+      </ul>
+    </nav>
+  );
 }
