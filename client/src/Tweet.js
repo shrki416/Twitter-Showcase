ed00f21d52efecef1a73852815eb9d2bd871f7cd
shrki416 <coach-ahmed@bmsnj.org>
2020-02-18 15:55:58 -0500

    Display tweet info on card

--- client/src/Tweet.js
+++ client/src/Tweet.js
@@ -4,19 +4,21 @@ import "./Tweet.css";
 function Tweet(props) {
   return (
     <div className="card">
-      <h4>{props.name}</h4>
-      <h5>@{props.screen_name}</h5>
-      <h6>{props.created}</h6>
-      <img className="circle-img" src={props.img} alt="user-image" />
-      <p>{props.text}</p>
-      <p>
-        <i className="fas fa-retweet"></i>
-        {props.retweet}
-      </p>
-      <p>
-        <i className="fas fa-heart"></i>
-        {props.favorite}
-      </p>
+      <div className="profile-sidebar">
+        <img className="profile-image" src={props.img} alt="user-image" />
+      </div>
+      <div className="profile-main">
+        <h2 className="profile-name">{props.name}</h2>
+        <p className="profile-position">@{props.screen_name}</p>
+        <p className="profile-position">{props.created}</p>
+        <p className="profile-body">{props.text}</p>
+        <p className="tweet-stats">
+          <i className="fas fa-retweet"></i> {props.retweet}
+        </p>
+        <p className="tweet-stats">
+          <i className="fas fa-heart"></i> {props.favorite}
+        </p>
+      </div>
     </div>
   );
 }
