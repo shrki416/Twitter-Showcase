f51322119425b71927b0672aeb77c9fe17f2ab66
shrki416 <coach-ahmed@bmsnj.org>
2020-02-21 11:56:09 -0500

    Change Random.js page structure

--- client/src/Random.js
+++ client/src/Random.js
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import SportsBaseballIcon from "@material-ui/icons/SportsBaseball";
+import SportsBasketballIcon from "@material-ui/icons/SportsBasketball";
+import SportsSoccerIcon from "@material-ui/icons/SportsSoccer";
+import SportsHockeyIcon from "@material-ui/icons/SportsHockey";
+import SportsFootballIcon from "@material-ui/icons/SportsFootball";
 import Header from "./Header";
 import axios from "axios";
 import "./Random.css";
@@ -9,6 +14,14 @@ const Random = () => {
     image: ""
   });
 
+  const iconStyle = {
+    color: "var(--primary)",
+    fontSize: "100px",
+    border: "2px solid var(--primary)",
+    borderRadius: "10px",
+    margin: "2%"
+  };
+
   return (
     <>
       <div className="random-container">
@@ -17,7 +30,28 @@ const Random = () => {
         <p className="random-header-p">
           Click on the logo below to view a randomly selected tweet
         </p>
-        <section></section>
+        <section>
+          <div>
+            <SportsBaseballIcon style={iconStyle} />
+            <p className="screen-name">@MLB</p>
+          </div>
+          <div>
+            <SportsBasketballIcon style={iconStyle} />
+            <p className="screen-name">@NBA</p>
+          </div>
+          <div>
+            <SportsSoccerIcon style={iconStyle} />
+            <p className="screen-name">@MLS</p>
+          </div>
+          <div>
+            <SportsFootballIcon style={iconStyle} />
+            <p className="screen-name">@NFL</p>
+          </div>
+          <div>
+            <SportsHockeyIcon style={iconStyle} />
+            <p className="screen-name">@NHL</p>
+          </div>
+        </section>
       </div>
     </>
   );
