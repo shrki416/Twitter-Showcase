5fec1e4dae52a67eaaf896e402d8aaa13913184a
shrki416 <coach-ahmed@bmsnj.org>
2020-02-21 10:08:52 -0500

    Change component from Class to Functional

--- client/src/Random.js
+++ client/src/Random.js
@@ -1,72 +1,26 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import axios from "axios";
 import "./Random.css";
 
-const MLB = require("./assets/MLB.png");
-const NHL = require("./assets/NHL.png");
-const NFL = require("./assets/NFL.png");
-const MLS = require("./assets/MLS.png");
-const NBA = require("./assets/NBA.png");
+const Random = () => {
+  const [user, setUser] = useState({
+    username: "",
+    image: ""
+  });
 
-const handleClick = e => {
-  const user = e.target.alt;
-  console.log(user);
+  return (
+    <>
+      <div className="random-container">
+        <Header />
+        <h1 className="random-header">Random Tweet Page</h1>
+        <p className="random-header-p">
+          Click on the logo below to view a randomly selected tweet
+        </p>
+        <section></section>
+      </div>
+    </>
+  );
 };
 
-class Random extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-
-    console.log(props.tweets);
-  }
-
-  render() {
-    return (
-      <>
-        <div className="random-container">
-          <Header />
-          <h1 className="random-header">Random Tweet Page</h1>
-          <p className="random-header-p">
-            Click on the logo below to view a randomly selected tweet
-          </p>
-          <section className="random-section">
-            <img
-              onClick={handleClick}
-              className="random-img"
-              src={MLB}
-              alt="MLB-Logo"
-            />
-            <img
-              onClick={handleClick}
-              className="random-img"
-              src={NHL}
-              alt="NHL-Logo"
-            />
-            <img
-              onClick={handleClick}
-              className="random-img"
-              src={NBA}
-              alt="NBA-Logo"
-            />
-            <img
-              onClick={handleClick}
-              className="random-img"
-              src={MLS}
-              alt="MLS-Logo"
-            />
-            <img
-              onClick={handleClick}
-              className="random-img"
-              src={NFL}
-              alt="NFL-Logo"
-            />
-          </section>
-        </div>
-      </>
-    );
-  }
-}
 export default Random;
