033f168aadfbb29119ca4c658bce133da3e7c95d
shrki416 <coach-ahmed@bmsnj.org>
2020-02-26 11:27:08 -0500

    code refactor

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "client/build")));
-
 app.get("/api/search", (req, res) => {
   const url = `https://api.twitter.com/1.1/search/tweets.json?q=${req.query.search_term}&count=6&result_type=popular`;
 
@@ -53,5 +51,7 @@ app.get("/api/random", (req, res) => {
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
+
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.listen(port, () => console.log(`Server Started on port ${port}!`));
