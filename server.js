0b01f0370184cf361b71f61cbf9057aacc0f338c
shrki416 <coach-ahmed@bmsnj.org>
2020-02-26 11:51:38 -0500

    restore server.js file back to original

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, "client/build")));
+
 app.get("/api/search", (req, res) => {
   const url = `https://api.twitter.com/1.1/search/tweets.json?q=${req.query.search_term}&count=6&result_type=popular`;
 
@@ -51,7 +53,5 @@ app.get("/api/random", (req, res) => {
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
-
-app.use(express.static(path.join(__dirname, "client/build")));
 
 app.listen(port, () => console.log(`Server Started on port ${port}!`));
