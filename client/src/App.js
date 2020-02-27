8a2d44edb5f2db0d24f1d9978a51b503a0a90008
shrki416 <coach-ahmed@bmsnj.org>
2020-02-26 21:50:30 -0500

    App.js refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,20 @@
 import React, { Component } from "react";
-import axios from "axios";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./Home";
 import Search from "./Search";
 import Random from "./Random";
 
-class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      tweets: []
-    };
-  }
-
-  render() {
-    return (
-      <>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route
-              exact
-              path="/search"
-              render={props => <Search tweets={this.state.tweets} {...props} />}
-            />
-            <Route
-              exact
-              path="/random"
-              render={props => <Random tweets={this.state.tweets} {...props} />}
-            />
-            {/* <Route component={} /> */}
-          </Switch>
-        </Router>
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/random" component={Random} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
