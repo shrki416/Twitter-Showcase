dab2173b27b64511f4ff842b7ae209f853a439ac
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-03-10 20:58:17 -0500

    Refactor API routes

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const axios = require("axios");
 const path = require("path");
-const tokenService = require("./token-service");
+
 require("dotenv").config();
 
 const app = express();
@@ -9,59 +8,11 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get("/api/search", async (req, res) => {
-  const url = `https://api.twitter.com/1.1/search/tweets.json`;
+const apiRoutes = require("./routes");
+app.use("/api", apiRoutes);
 
-  const token = await tokenService.getToken();
-
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`
-    },
-    params: {
-      q: req.query.search_term,
-      count: 6,
-      result_type: "popular"
-    }
-  };
-  axios
-    .get(url, config)
-    .then(response => {
-      res.send(response.data);
-    })
-    .catch(error => {
-      console.log(`Something went wrong: ${error}`);
-      res.sendStatus(500);
-    });
-});
-
-app.get("/api/random", async (req, res) => {
-  const url = `https://api.twitter.com/1.1/statuses/user_timeline.json`;
-
-  const token = await tokenService.getToken();
-
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`
-    },
-    params: {
-      screen_name: req.query.screen_name,
-      count: 6
-    }
-  };
-  axios
-    .get(url, config)
-    .then(response => {
-      res.send(response.data);
-    })
-    .catch(error => {
-      console.log(`Something went wrong: ${error}`);
-      res.sendStatus(500);
-    });
-});
-
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+app.get("/*", (req, res) =>
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+);
 
 app.listen(port, () => console.log(`Server Started on port ${port}!`));
